b9081026341eefdb77a6bf9d5e3a784d58e58983
*qB8tel7hwv <omar.oportober@gmail.com>
2024-11-21 15:57:51 -0400

    minor changes

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from users.models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
-    # Opcional: Personaliza los campos que deseas mostrar
+
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('role',)}),
     )
