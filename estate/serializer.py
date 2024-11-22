5e9d5bea2d76364ec7279d3d8b05eb70631af5a1
AliCuchalloGabrielFerando <f1.gabriel73@gmail.com>
2024-11-22 12:49:03 -0400

    fix problem mi owner_id

--- estate/serializer.py
+++ estate/serializer.py
@@ -1,14 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from users.models import User
 from estate.models import Estate
-
+from rest_framework import serializers
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username']
 
 class EstateSerializer(ModelSerializer):
-    owner_id = UserSerializer(read_only=True)
+    owner_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
     class Meta:
         model = Estate
         fields= ['id','name','rooms','bathrooms','floors','price','is_available','owner_id']
@@ -17,3 +17,10 @@ class EstateSerializer(ModelSerializer):
         validated_data.pop('is_available', None)  # Esto asegura que se use el valor por defecto
         estate = Estate.objects.create(**validated_data)
         return estate
+
+    def to_representation(self, instance):
+        # Sobreescribe el método `to_representation` para incluir la información del `User` en el `owner_id`
+        representation = super().to_representation(instance)
+        # Serializa el objeto `User` en lugar de solo su ID
+        representation['owner_id'] = UserSerializer(instance.owner_id).data
+        return representation
